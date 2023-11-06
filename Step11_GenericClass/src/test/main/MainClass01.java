1b6b7d4a951d97b575955ab63b42e2180a8ed1c7
kimgura <oli999@naver.com>
2023-11-06 10:53:57 +0900

    Step11 에 필기 추가함

--- Step11_GenericClass/src/test/main/MainClass01.java
+++ Step11_GenericClass/src/test/main/MainClass01.java
@@ -2,6 +2,7 @@ package test.main;
 
 import test.mypac.Apple;
 import test.mypac.FruitBox;
+import test.mypac.Melon;
 import test.mypac.Orange;
 
 public class MainClass01 {
@@ -10,6 +11,28 @@ public class MainClass01 {
 		FruitBox<Apple> box=new FruitBox<Apple>();
 		FruitBox<Orange> box2=new FruitBox<Orange>();
 		
+		//객체 생성시에 Generic type 은 생략 가능하다
+		FruitBox<Melon> box3=new FruitBox<>();
+		
+		//Generic 클래스가 Apple 로 되어있는 객체 사용하기 
+		box.pack(new Apple()); //Apple type 을 매개변수에 전달한다.
+		Apple f1=box.unPack(); //Apple type 이 리턴된다.
+		
+		//Generic 클래스가 Orange 로 되어 있는 객체 사용하기
+		box2.pack(new Orange()); //Orange type 을 매개 변수에 전달한다.
+		Orange f2=box2.unPack(); //Orange type 이 리턴된다.
+		
 		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
