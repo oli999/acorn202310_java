f516511a25c49587d4ee943e1e3efb24218c6fd3
kimgura <oli999@naver.com>
2023-11-07 17:41:24 +0900

    Step13_Exception 프로젝트 추가함

---
+++ Step13_Exception/src/test/main/QuizMain2.java
@@ -0,0 +1,22 @@
+package test.main;
+/*
+ *  run 했을때 콘솔창에 경과 시간이 출력되는 프로그래밍을 해 보세요.
+ *  
+ *  0 분 1초
+ *  0 분 2초
+ *  0 분 3초
+ *  .
+ *  .
+ *  0 분 59초
+ *  1 분 0초
+ *  1 분 1초
+ *  .
+ *  .
+ *  2 분 0초
+ *  2 분 1초
+ */
+public class QuizMain2 {
+	public static void main(String[] args) {
+		
+	}
+}
